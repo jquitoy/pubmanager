32227d965d96b0e9cd65399087769174991fbf66
joshuaquitoy22@gmail.com <joshuaquits@gmail.com>
2025-05-23 23:57:01 +0800

    added roles and staffs table

--- pub/models.py
+++ pub/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Roles(models.Model):
+  class Meta:
+    db_table = 'tbl_roles'
+
+  staff_id = models.BigAutoField(primary_key=True, blank=False)
+  role = models.CharField(max_length=55, blank=False)
+
+class Staffs(models.Model):
+  class Meta:
+    db_table = 'tbl_staffs'
+
+  staff_id = models.BigAutoField(primary_key=True, blank=False)
+  full_name = models.CharField(max_length=55, blank=False)
+  position = models.CharField(max_length=55, blank=False)
+  email = models.EmailField(max_length=55, blank=False)
+  role = models.ManyToManyField(Roles) 
+  google_calendar_id = models.CharField(
+    max_length=255,
+    blank=True,
+    default='primary'  # Default to their primary calendar
+  )
+ 
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
