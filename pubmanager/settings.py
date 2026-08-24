c9224e4a20a637b01666325ba8acc66cef17fdf5
joshuaquitoy22@gmail.com <joshuaquits@gmail.com>
2026-08-25 00:54:41 +0800

    test

--- pubmanager/settings.py
+++ pubmanager/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -181,6 +182,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'pub' / 'static']
+STATICFILES_EXCLUDES = ['css/input.css']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Security for production behind proxy (Vercel)
 if not DEBUG:
